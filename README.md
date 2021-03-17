16dcdf5ceaaa3ff23fb6eac704c40044fed236f8
Jason Wright <jwright@speareducation.com>
2021-03-17 14:46:28 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
-# Spear Education Build Variables
+# Spear Education Check Code Coverage
 *Note: This action is designed to be compatible within Spear's ecosystem.
 
-Parses the action environment
-
-## Outputs
-
-### `release`
-The release name, taken from the branch or tag.
-Examples:
-- `2021-12-01.01`
-- `gl-1234`
-
-### `appEnv`
-The APP Environment, taken from the branch or tag.
-Examples:
-- `staging`
-- `sandbox`
-- `dotco`
-- `production`
-
-## Example usage
-```
-- id: build-vars
-  uses: speareducation/action-build-variables@main
-
-- id: other-action
-  env:
-    release: ${{ steps.build-vars.outputs.release }}
-    appEnv: ${{ steps.build-vars.outputs.appEnv }}
-```
+Checks code coverage against another branch and outputs the difference
--- index.js
+++ index.js
@@ -1,17 +1,67 @@
+const AWS = require('aws-sdk');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
-const tag = github.context.ref.replace(/^refs\/(heads|tags)\//, '')
-const [ text1, appEnv, release ] = tag.split('/');
-console.log({ tag, appEnv, release })
+const inputBucket = core.getInput('bucket');
+const inputLocalFile = core.getInput('localFile');
+const inputRemoteFile = core.getInput('remoteFile');
 
+/**
+ * Takes a coverage.txt or jest-coverage.txt file contents and outputs a Number representing the line coverage percent.
+ * @param {String} fileContents 
+ * @return {Number} coverage
+ */
+ function parseCoverage(fileContents) {
+    try {
+        // matches jest and phpunit coverage patterns:
+        //|Lines        : 17.78% ( 210/1181 )
+        //|  Lines:   90.44% (1060/1172)
+        const [line, coverage] = fileContents.toString().match(/^ *Lines *: *([\d\.]+)%/m);
+        return Number(coverage);
 
-if (!/^(sandbox|dotco|uat|staging|production)$/.test(appEnv)) {
-    core.setFailed('appEnv could not be parsed from the ref, or it is invalid.')
+    } catch (error) {
+        console.warn(error);
+        return 0;
+    }
 }
-if (!release) {
-    core.setFailed('release could not be parsed from the ref.');
+
+/**
+ * Main Handler
+ */
+async function handle() {
+    let remoteCoverage = 0;
+    let localCoverage = 0;
+
+    try {
+        const rsp = fs.readFileSync(inputLocalFile);
+        localCoverage = parseCoverage(rsp.toString());
+    } catch (error) {
+        console.warn(error);
+    }
+
+    try {
+        const S3 = new AWS.S3();
+        const rsp = await S3.getObject({ Bucket: inputBucket, Key: inputRemoteFile }).promise();
+        remoteCoverage = parseCoverage(rsp.Body.toString())
+    } catch (error) {
+        console.warn(error);
+    }
+
+    const difference = Math.round(localCoverage - remoteCoverage, 2);
+    let description;
+
+    if (difference === 0.00) {
+        description = 'Coverage is unchanged.';
+    } else if (difference > 0) {
+        description = `Coverage up by ${difference}%`;
+    } else {
+        description = `Coverage down by ${difference}%`;
+    }
+
+    core.setOutput('description', description);
+    core.setOutput('difference', difference > 0 ? `Coverage up by ${difference}%` : `Coverage down by ${difference}%`)
+
 }
 
-core.setOutput('appEnv', appEnv);
-core.setOutput('release', release);
+handle().catch(error => core.setFailed(error));
